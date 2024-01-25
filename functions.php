d9d937e216e7497c6ad65ed72f9c6c6f9990a211
Stephen <stephen@behnfeldt.pro>
2024-01-25 09:06:27 -0500

    Lesson 34) Navbar & Widgets Part B Added widgets. Tried to customize them but not working as shown in the tutorial.

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once 'class-wp-bootstrap-navwalker.php';
+require_once 'widgets/class-wp-widget-categories.php';
+require_once 'widgets/class-wp-widget-recent-comments.php';
+require_once 'widgets/class-wp-widget-recent-posts.php';
+
 
 function theme_setup() {
     // Featured Image Support
@@ -28,3 +32,22 @@ function init_widgets( $id ) {
 }
 
 add_action( 'widgets_init', 'init_widgets' );
+
+// Adds 'list-group-item' to categories <li>
+function add_new_class_list_categories( $list ) {
+    $list = str_replace( 'cat-item', 'cat-item list-group-item', $list );
+
+    return $list;
+}
+
+add_filter( 'wp_list_categories', 'add_new_class_list_categories' );
+
+// Register Widgets
+function wordstrap_register_widgets()
+{
+    register_widget('WP_Widget_Recent_Posts_Custom');
+    register_widget('WP_Widget_Recent_Comments_Custom');
+    register_widget('WP_Widget_Categories_Custom');
+}
+
+add_action('widgets_init', 'wordstrap_register_widgets');
