c6ab1f7904d51a534aef9a0c2108180e9e449758
Stephen <stephen@behnfeldt.pro>
2024-01-25 07:18:36 -0500

    Lesson 33) Navbar & Widgets Part A Added "navwalker" class for menu (current navwalker does not work using the instructions in this old video). Added support for widgets to theme.

--- functions.php
+++ functions.php
@@ -1,8 +1,30 @@
 <?php
 
-function theme_setup( ) {
+require_once 'class-wp-bootstrap-navwalker.php';
+
+function theme_setup() {
     // Featured Image Support
-    add_theme_support('post-thumbnails');
+    add_theme_support( 'post-thumbnails' );
+
+    // Nav Menus
+    register_nav_menus( [
+        'primary' => __( 'Primary Menu' )
+    ] );
 }
 
-add_action('after_setup_theme', 'theme_setup');
+add_action( 'after_setup_theme', 'theme_setup' );
+
+// Widget Locations
+function init_widgets( $id ) {
+    register_sidebar( [
+        'name'          => 'Sidebar',
+        'id'            => 'sidebar',
+        'before_widget' => '<div class="card card-default">',
+        'after_widget'  => '</div></div>',
+        'before_title'  => '<div class="card-heading"><h3 class="card-title">',
+        'after_title'   => '</h3></div><div class="card-body">'
+
+    ] );
+}
+
+add_action( 'widgets_init', 'init_widgets' );
--- partials/header.php
+++ partials/header.php
@@ -8,8 +8,14 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-
+                <?php wp_nav_menu( array(
+                    'theme_location'  => 'primary',
+                    'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                    'container'       => false,
+                    'menu_class'      => 'navbar-nav mr-auto',
+                    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'          => new WP_Bootstrap_Navwalker(),
+                ) ); ?>
                 <form class="d-flex" role="search"></form>
             </div>
         </div>
