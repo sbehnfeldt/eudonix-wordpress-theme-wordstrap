8200e1ac7dc55d294bc3044b00e9defb4e8c675a
Stephen <stephen@behnfeldt.pro>
2024-01-25 06:06:15 -0500

    Lesson 32) The Post Loop Wordstrap Theme Added theme support for featured image.  Display posts page using Bootstrap grid.  Display posts according to whether they have a featured image.

--- index.php
+++ index.php
@@ -11,17 +11,50 @@
                 <div class="card-heading">
                     <h3 class="card-title">Blog Posts</h3>
                 </div>
+
                 <div class="card-body">
                     <?php if ( have_posts() ): ?>
                         <?php while ( have_posts() ): the_post(); ?>
-                            <div class="row">
-                                <div class="col-md-3">
-                                    IMAGE HERE
+                            <article class="post">
+                                <div class="row">
+                                    <?php if ( has_post_thumbnail() ): ?>
+                                        <div class="col-md-3">
+                                            <div class="post-thumbnail">
+                                                <?php the_post_thumbnail(); ?>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-9">
+                                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                            <p class="meta">
+                                                Posted at <?php the_time(); ?>
+                                                on <?php the_date(); ?>
+                                                by <strong><?php the_author(); ?></strong>
+                                            </p>
+                                            <div class="excerpt">
+                                                <?php the_excerpt(); ?>
+                                                <br>
+                                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More &raquo;</a>
+                                            </div>
+                                        </div>
+                                    <?php else: ?>
+                                        <div class="col-md-12">
+                                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                            <p class="meta">
+                                                Posted at <?php the_time(); ?>
+                                                on <?php the_date(); ?>
+                                                by <strong><?php the_author(); ?></strong>
+                                            </p>
+                                            <div class="excerpt">
+                                                <?php the_excerpt(); ?>
+                                                <br>
+                                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More &raquo;</a>
+                                            </div>
+
+                                        </div>
+                                    <?php endif; ?>
                                 </div>
-                                <div class="col-md-9">
-                                    <h2><?php the_title(); ?></h2>
-                                </div>
-                            </div>
+                            </article>
                         <?php endwhile; ?>
                     <?php endif; ?>
                 </div>
